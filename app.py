ec8a8bdc7214723d09c4ef663ae3953c801f4b69
Chuyang <vcyreg@163.com>
2021-12-28 00:16:01 -0500

    score

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from subprocess import check_output
 from urllib.parse import urlparse
 
 import flask
-import pandas
+import pandas as pd
 import requests
 import sklearn
 from flask import Flask, request
@@ -57,12 +57,14 @@ def build_model():
     predictors = body['predictors']
     target = body['target']
     model_name = body['name']
+    folder_id = body['folderId']
     description = body['description']
     function = body['function']
+    algorithm = body['algorithm']
 
     token = request.headers.get('Authorization')
     resp = requests.get(train_table, headers={"Authorization": token})
-    df = pandas.read_csv(StringIO(resp.text))
+    df = pd.read_csv(StringIO(resp.text))
     try:
         x = df.loc[:, predictors]
         y = df.loc[:, target]
@@ -89,8 +91,10 @@ def build_model():
     }
     data = {
         'name': model_name,
+        'folderId': folder_id,
         'description': description,
-        'function': function
+        'function': function,
+        'algorithm': algorithm
     }
     response = requests.post(host_port + '/models', files=file_dict, data=data, headers={"Authorization": token})
     print('response.content:' + response.content.decode('utf-8'))
@@ -100,6 +104,58 @@ def build_model():
         mimetype='application/json',
         status=201
     )
+    return response
+
+# serve form request, example:
+# file: pickle file
+# scoreOutputTable: table name
+# scoreInputTable: in form of http://127.0.0.1:8000/dataSources/xxxxxx
+# drawerId: drawerId
+@app.route('/score', methods=['POST'])
+def score():
+    model_pickle = request.files['file']
+
+    score_input_table = request.form['scoreInputTable']
+    score_output_table_name = request.form['scoreOutputTable']
+    drawer_id = request.form['drawerId']
+    token = request.headers.get('Authorization')
+    resp = requests.get(score_input_table + "/content", headers={"Authorization": token})
+    scoring_input = pd.read_csv(StringIO(resp.text))
+    print('score output table shape:' + ','.join(str(x) for x in scoring_input.shape))
+
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_file_path = os.path.join(temp_dir.name, secure_filename(model_pickle.filename))
+    model_pickle.save(temp_file_path)
+    print('model pickle file:' + temp_file_path)
+    model = pickle.load(open(temp_file_path, 'rb'))
+    predictions = model.predict(scoring_input)
+
+    predictions = pd.DataFrame(predictions, columns=['output'])
+    scoring_input = scoring_input.reset_index(drop=True)
+    result = pd.concat([scoring_input, predictions], axis=1)
+    scoring_output_file_name = os.path.join(temp_dir.name, secure_filename(score_output_table_name + '.csv'))
+    print('output file:' + scoring_output_file_name)
+    result.to_csv(scoring_output_file_name)
+
+    host_port = _get_host(score_input_table)
+    print('host and port:' + host_port)
+    file_dict = {
+        'file': (score_output_table_name + '.csv', open(scoring_output_file_name, 'rb').read())
+    }
+    data = {
+        'drawerId': drawer_id
+    }
+    response = requests.post(host_port + '/dataSources', files=file_dict, data=data, headers={"Authorization": token})
+    print('response.content:' + response.content.decode('utf-8'))
+    json_object = json.loads(response.content)
+    response = app.response_class(
+        response=json.dumps(json_object, indent=2),
+        mimetype='application/json',
+        status=201
+    )
+
+    os.remove(scoring_output_file_name)
+
     return response
 
 
